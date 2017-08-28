7106f8ee01582678d3e81b2d30f6545635fd5600
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-08-28 10:54:17 +0500

    Implement mathematical operations

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
 // eslint-disable-next-line
+Vue.config.productionTip = false
+// eslint-disable-next-line
 var myApp = new Vue({
   el: '#calculator',
   data: {
@@ -7,48 +9,61 @@ var myApp = new Vue({
     lastChar: '',
     char: '',
     result: 0,
-    expression: ''
+    expression: '4+2x10/5-4'
   },
   created () {},
   watch: {},
   computed: {},
   methods: {
+    /**
+     * @description Handles click event on digits, functions and operators
+     * @param {any} e Click event
+     */
     handleClick (e) {
       this.lastChar = this.char
       this.char = e.target.innerHTML
       if (this.digits.indexOf(this.char) > -1) {
-        this.handleDigit()
+        this.addDigit()
       } else if (this.operators.indexOf(this.char) > -1) {
-        this.handleOperator()
+        this.addOperator()
       } else {
-        this.handleFunction()
+        this.addFunction()
       }
     },
-    handleDigit () {
+    /**
+     * @descriptionInsert digits into experssion
+     */
+    addDigit () {
       if (this.char !== '.') {
         this.expression += this.char
       } else if (this.expression.indexOf(this.char) === -1) {
         this.expression += this.char
       }
     },
-    handleOperator () {
+    /**
+     * @description Insert operator into expression
+     */
+    addOperator () {
       if (this.operators.indexOf(this.lastChar) > -1) {
         this.expression = this.expression.slice(0, -1)
       }
       this.expression += this.char
     },
-    handleFunction () {
+    /**
+     * @description Perform functions
+     */
+    addFunction () {
       switch (this.char) {
         case 'CE':
           this.expression = ''
+          this.result = 0
           break
         case 'C':
           let len = this.expression.length
           if (this.operators.indexOf(this.expression[len - 1]) > -1) {
             this.expression = this.expression.slice(0, -1)
           } else {
-            // eslint-disable-next-line
-						var li = this.expression.lastIndexOf(this.expression.split(/[x/\+-]/gi).pop())
+            let li = this.expression.lastIndexOf(this.getNumberFromExp(this.expression, false))
             this.expression = this.expression.slice(0, li - len)
           }
           break
@@ -59,8 +74,73 @@ var myApp = new Vue({
           this.result *= -1
           break
         case '=':
-          // Calculate result
-          break
+          let tmpExp = this.expression
+
+          this.operators.split('').forEach(operator => {
+            let parts = tmpExp.split(new RegExp('[' + operator + ']', 'gi'))
+
+            let len = parts.length
+            let first
+            let last
+            let resExp = ''
+            let res = 0
+
+            if (len > 1) {
+              parts.forEach((part, index) => {
+                if (index === 0) {
+                  last = this.getNumberFromExp(part, false)
+                  part = part.substr(0, part.length - last.toString().length)
+                } else if (index === len - 1) {
+                  first = this.getNumberFromExp(part, true)
+                  part = part.substr(first.toString().length)
+                } else {
+                  first = this.getNumberFromExp(part, true)
+                  last = this.getNumberFromExp(part, false)
+                  part = part.substr(first.toString().length)
+                  part = part.substr(0, part.length - last.toString().length)
+                }
+
+                if (first && last) {
+                  switch (operator) {
+                    case 'x':
+                      res = last * first
+                      break
+                    case '/':
+                      res = last / first
+                      break
+                    case '+':
+                      res = last + first
+                      break
+                    case '-':
+                      res = last - first
+                      break
+                  }
+                }
+
+                resExp += res.toString() + part
+
+                console.log(resExp)
+              })
+
+              tmpExp = resExp
+            }
+          })
+
+          this.result = parseFloat(tmpExp, 10)
+      }
+    },
+    /**
+     * @description Get last or first number from a given expression
+     * @param {string} exp Expression
+     * @param {boolean} first First (true) or Last (false) number in expression
+     */
+    getNumberFromExp (exp, first) {
+      // eslint-disable-next-line
+      let numbers = exp.split(/[x/\+-]/gi)
+      if (first) {
+        return parseFloat(numbers.shift(), 10)
+      } else {
+        return parseFloat(numbers.pop(), 10)
       }
     }
   }
